bbcd888716df268dbe725592ad9b5a4df0073cf8
Danylo Bezukh <113092192+DaniloBez@users.noreply.github.com>
2026-05-25 13:38:26 +0300

    Fix: add Fake receiver & sender

---
+++ src/main/java/sender/FakeSender.java
@@ -0,0 +1,26 @@
+package sender;
+
+import decryptor.IDecryptor;
+import decryptor.MessageDecryptor;
+import dto.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetAddress;
+
+public class FakeSender implements ISender {
+    private static final Logger logger = LoggerFactory.getLogger(FakeSender.class);
+
+    private final IDecryptor clientDecryptor = new MessageDecryptor();
+
+    @Override
+    public void send(byte[] message, InetAddress target) {
+        try {
+            Message responseMessage = clientDecryptor.decrypt(message);
+
+            logger.info("Sending message: {} ", responseMessage);
+        } catch (Exception e) {
+            logger.error("Decryption error on the client: {}", e.getMessage());
+        }
+    }
+}
